ffa47d1b1517a290a943688f724267781b54757d
ak48disk <ak48disk@gmail.com>
2015-02-03 02:11:34 +0000

    111

---
+++ Game.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml;
+
+namespace ConnectFour
+{
+    class Game
+    {
+        public enum PlayerType
+        {
+            Empty,
+            FirstPlayer,
+            SecondPlayer
+        }
+
+        public struct Point
+        {
+            public int Col;
+            public int Row;
+
+            public Point(int col, int row)
+            {
+                Col = col;
+                Row = row;
+            }
+        }
+
+        private PlayerType[,] board;
+        public const int nRows = 6;
+        public const int nCols = 7;
+        private List<List<Point>> lines = new List<List<Point>>();
+        
+        public Game()
+        {
+            InitializeLines();
+            Reset();
+        }
+
+        public void Reset()
+        {
+            board = new PlayerType[nCols, nRows];
+            
+            for (int i = 0; i < nCols; ++i)
+            {
+                for (int j = 0; j < nRows; ++j)
+                {
+                    board[i, j] = PlayerType.Empty;
+                }
+            }
+        }
+
+        public PlayerType GetPiece(int nCol, int nRow)
+        {
+            return board[nCol, nRow];
+        }
+
+        private void InitializeLines()
+        {
+            List<Point> currentLine;
+            for (int i = 0; i < nCols; ++i)
+            {
+                currentLine = new List<Point>();
+                for (int j = 0; j < nRows; ++j)
+                {
+                    currentLine.Add(new Point(i, j));
+                }
+                lines.Add((currentLine));
+            }
+            for (int j = 0; j < nRows; ++j)
+            {
+                currentLine = new List<Point>();
+                for (int i = 0; i < nCols; ++i)
+                {
+                    currentLine.Add(new Point(i, j));
+                }
+                lines.Add((currentLine));
+            }
+
+            for (int j = 0; j < nRows; ++j)
+            {
+                Point p = new Point(0, j);
+                currentLine = new List<Point>();
+                while (p.Col >= 0 && p.Col < nCols && p.Row >= 0 && p.Row < nRows)
+                {
+                    currentLine.Add(p);
+                    p.Col ++;
+                    p.Row ++;
+                }
+                lines.Add(currentLine);
+
+                p = new Point(0, j);
+                currentLine = new List<Point>();
+                while (p.Col >= 0 && p.Col < nCols && p.Row >= 0 && p.Row < nRows)
+                {
+                    currentLine.Add(p);
+                    p.Col++;
+                    p.Row--;
+                }
+                lines.Add(currentLine);
+
+                p = new Point(nCols - 1, j);
+                currentLine = new List<Point>();
+                while (p.Col >= 0 && p.Col < nCols && p.Row >= 0 && p.Row < nRows)
+                {
+                    currentLine.Add(p);
+                    p.Col--;
+                    p.Row++;
+                }
+                lines.Add(currentLine);
+
+                p = new Point(nCols - 1, j);
+                currentLine = new List<Point>();
+                while (p.Col >= 0 && p.Col < nCols && p.Row >= 0 && p.Row < nRows)
+                {
+                    currentLine.Add(p);
+                    p.Col--;
+                    p.Row--;
+                }
+                lines.Add(currentLine);
+            }
+            lines = lines.Where(r => r.Count >= 4).ToList();
+        }
+
+        public bool PlayAt(PlayerType player, int nCol)
+        {
+            if (nCol < 0 || nCol >= nCols) 
+                return false;
+            for (int i = 0; i < nRows; ++i)
+            {
+                if (board[nCol, i] == PlayerType.Empty)
+                {
+                    board[nCol, i] = player;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public int CalculateSets(PlayerType player)
+        {
+            int sets = 0;
+            foreach (var line in lines)
+            {
+                int pieceInLine = 0;
+                foreach (var piece in line)
+                {
+                    if (board[piece.Col, piece.Row] == player)
+                        pieceInLine ++;
+                    else
+                        pieceInLine = 0;
+                    if (pieceInLine >= 4)
+                    {
+                        sets ++;
+                        break;
+                    }
+                }
+            }
+            return sets;
+        }
+
+        public int CalculateNumberOfPieces(PlayerType player)
+        {
+            int pieces = 0;
+            for (int i = 0; i < nCols; ++i)
+            {
+                for (int j = 0; j < nRows; ++j)
+                {
+                    if (board[i, j] == player)
+                        pieces++;
+                }
+            }
+            return pieces;
+        }
+
+        public int CalculateScore(PlayerType player)
+        {
+            if (player == PlayerType.Empty)
+                throw new Exception();
+            PlayerType opponent = player == PlayerType.FirstPlayer ? PlayerType.SecondPlayer : PlayerType.FirstPlayer;
+            return CalculateNumberOfPieces(player) + Math.Max(0, 4 * CalculateSets(player) - 2 * CalculateSets(opponent));
+        }
+
+        public void Load(string FileName)
+        {
+            using (FileStream fs = new FileStream(FileName, FileMode.Open))
+            {
+                using (StreamReader sw = new StreamReader(fs))
+                {
+                    for (int i = 0; i < nCols; ++i)
+                    {
+                        for (int j = 0; j < nRows; ++j)
+                        {
+                            board[i, j] = (PlayerType)sw.Read();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Save(string FileName)
+        {
+            using (FileStream fs = new FileStream(FileName, FileMode.CreateNew))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    for (int i = 0; i < nCols; ++i)
+                    {
+                        for (int j = 0; j < nRows; ++j)
+                        {
+                            sw.Write((char)board[i, j]);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectFour
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private Game theGame = new Game();
+        private Ellipse[,] pieces;
+
+        private void AddPieces()
+        {
+            pieces = new Ellipse[Game.nCols, Game.nRows];
+            const int size = 80;
+            for(int i=0; i< Game.nCols; ++i)
+            {
+                for (int j=0; j < Game.nRows; ++j)
+                {
+                    pieces[i, j] = new Ellipse();
+                    pieces[i, j].Width = pieces[i, j].Height = size;
+                    pieces[i, j].Fill = Brushes.Black;
+                    pieces[i, j].Tag = new Game.Point(i, j);
+                    pieces[i, j].MouseDown += Piece_Click;
+                    Canvas.SetLeft(pieces[i, j], i * size);
+                    Canvas.SetTop(pieces[i, j], (Game.nRows - j - 1) * size);
+                    theCanvas.Children.Add(pieces[i, j]);
+                }
+            }
+        }
+
+        private void Piece_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                theGame.PlayAt(Game.PlayerType.FirstPlayer, ((Game.Point)(sender as Ellipse).Tag).Col);
+            }
+            else
+            {
+                theGame.PlayAt(Game.PlayerType.SecondPlayer, ((Game.Point)(sender as Ellipse).Tag).Col);
+            }
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var colorMap = new Dictionary<Game.PlayerType, Brush> {
+                {Game.PlayerType.Empty, Brushes.Black},
+                {Game.PlayerType.FirstPlayer, Brushes.Red},
+                {Game.PlayerType.SecondPlayer, Brushes.Blue}
+            };
+            for (int i = 0; i < Game.nCols; ++i)
+            {
+                for (int j = 0; j < Game.nRows; ++j)
+                {
+                    //TODO
+                    pieces[i, j].Fill = colorMap[theGame.GetPiece(i, j)];
+                }
+            }
+            tb.Text = String.Format("Scores: {0}/{1}",theGame.CalculateScore(Game.PlayerType.FirstPlayer),
+                theGame.CalculateScore(Game.PlayerType.SecondPlayer));
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void theCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void ui_mnuNew_Click(object sender, RoutedEventArgs e)
+        {
+            theGame.Reset();
+            Refresh();
+        }
+
+        private void ui_mnuLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                theGame.Load(openFileDialog.FileName);
+                Refresh();
+            }
+        }
+
+        private void ui_mnuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                theGame.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AddPieces();
+        }
+
+
+    }
+}
